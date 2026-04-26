bca595aceb36852ce26be8d27c372006fff6852c
Nguyen Duc Chinh <nguyenducchinhdev@gmail.com>
2026-04-27 03:07:26 +0700

    feat: Implement VPC module with subnets, routing, and outputs

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+module "vpc" {
+  source = "./modules/vpc"
 
+  project_name         = var.project_name
+  aws_region           = var.aws_region
+  availability_zones   = var.availability_zones
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zone[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   tags = {
@@ -36,7 +36,7 @@ resource "aws_subnet" "private" {
   count                   = length(var.private_subnet_cidrs)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.private_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zone[count.index % length(var.availability_zone)]
+  availability_zone       = var.availability_zones[count.index % length(var.availability_zones)]
   map_public_ip_on_launch = false
 
   tags = {
